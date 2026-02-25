8b053ac9e7e57b84a5c9b86e0caedf5a2344091d
aryan983 <45117661+aryan983@users.noreply.github.com>
2026-02-25 20:33:34 +0000

    Added live train data

--- sw.js
+++ sw.js
@@ -17,8 +17,8 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   const url = new URL(e.request.url);
-  // Always go network-first for TFL API calls (live data)
-  if (url.hostname.includes('tfl.gov.uk')) {
+  // Always go network-first for live data APIs
+  if (url.hostname.includes('tfl.gov.uk') || url.hostname.includes('huxley2.azurewebsites.net')) {
     e.respondWith(
       fetch(e.request).catch(() =>
         new Response(JSON.stringify([]), { headers: { 'Content-Type': 'application/json' } })
